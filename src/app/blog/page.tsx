42b312ebbcc0d1e2739744c8664cbcfbe4152660
kaivi-duanxin <kaivi-duanxin@users.noreply.github.com>
2026-07-30 15:30:05 +0800

    update site domain to shizhou.info

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -462,7 +462,7 @@ export default function BlogPage() {
 							animate={{ opacity: 1, scale: 1 }}
 							whileHover={{ scale: 1.05 }}
 							whileTap={{ scale: 0.95 }}
-							href='https://kaivi.de5.net/'
+							href='https://shizhou.info/'
 							target='_blank'
 							className='card text-secondary static inline-flex items-center gap-2 rounded-xl px-4 py-2 text-xs'>
 							<JuejinSVG className='h-4 w-4' />
